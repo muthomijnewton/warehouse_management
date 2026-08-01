36f0d4294b5130769d8b1e8e5f4e13cb30a918f0
muthomijnewton <muthomijnewton@gmail.com>
2026-08-01 06:37:15 +0300

    Handle receipt, consume, and transfer stock movements

--- warehouse_management/warehouse_management/doctype/stock_entry/stock_entry.py
+++ warehouse_management/warehouse_management/doctype/stock_entry/stock_entry.py
@@ -1,9 +1,33 @@
 # Copyright (c) 2026, Newton Muthomi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class StockEntry(Document):
-	pass
+	def on_submit(self):
+		if self.purpose == "Receipt":
+			for row in self.items:
+				self.create_ledger_entry(
+					item=row.item,
+					warehouse=row.target_warehouse,
+					qty=row.quantity,
+					rate=row.rate,
+				)
+
+	def create_ledger_entry(self, item, warehouse, qty, rate):
+		ledger = frappe.new_doc("Stock Ledger Entry")
+
+		ledger.posting_date = self.posting_date
+		ledger.posting_time = self.posting_time
+		ledger.item = item
+		ledger.warehouse = warehouse
+		ledger.stock_entry = self.name
+
+		ledger.actual_quantity = qty
+		ledger.valuation_rate = rate
+		ledger.stock_value = qty * rate
+
+		ledger.insert(ignore_permissions=True)
+
